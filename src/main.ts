fa835d9bb38093ff5f2a2ef69523b6b0643ba76b
Mats Roshauw <mats.roshauw@appva.com>
2022-09-12 14:19:54 +0200

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
+import { loadFonts } from "./plugins/webfontloader";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
-import "./assets/main.css";
+loadFonts();
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+createApp(App)
+  .use(router)
+  .use(
+    createVuetify({
+      icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
+      components,
+      directives,
+    })
+  )
+  .mount("#app");
