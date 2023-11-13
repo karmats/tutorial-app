87b354af95bc369b3e21634c517e22a9f48ce7ae
Mats Roshauw <mats.roshauw@appva.com>
2023-11-13 21:22:59 +0100

    Custom xaxis labels

--- src/views/ApexGraphView.vue
+++ src/views/ApexGraphView.vue
@@ -21,6 +21,53 @@ import type { ApexOptions } from "apexcharts";
 import type { Ref } from "vue";
 import { getCurrentInstance, onMounted, ref, watch } from "vue";
 
+const categories = [
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1991, 0, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1991, 6, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1992, 0, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1992, 6, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1993, 0, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1993, 6, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1994, 0, 2),
+  ),
+  Intl.DateTimeFormat("sv", { year: "numeric", month: "short" }).format(
+    new Date(1994, 6, 2),
+  ),
+];
+const series1 = [30, 40, 45, 50, 49, 60, 70, 91];
+const series2 = [10, 20, 105, 40, 49, 50, 10, 67];
+const series = ref([
+  {
+    name: "series-1",
+    data: series1,
+  },
+  {
+    name: "series-2",
+    data: series2,
+  },
+]);
+const updateData = () => {
+  series.value = series.value.map((serie) => ({
+    ...serie,
+    data: serie.data.map(
+      (data) => Number(data) + Math.floor(Math.random() * 100),
+    ),
+  }));
+};
+
 const options: Ref<ApexOptions> = ref({
   chart: {
     id: "vuechart-example",
@@ -42,46 +89,14 @@ const options: Ref<ApexOptions> = ref({
       },
     },
   ],
-
   xaxis: {
-    type: "datetime",
-
+    type: "category",
     tooltip: {
       enabled: false,
     },
+    categories,
   },
 });
-const categories = [
-  new Date(1991, 0, 2),
-  new Date(1992, 0, 2),
-  new Date(1993, 0, 2),
-  new Date(1994, 0, 2),
-  new Date(1995, 0, 2),
-  new Date(1996, 0, 2),
-  new Date(1997, 0, 2),
-  new Date(1998, 0, 2),
-];
-const series1 = [30, 40, 45, 50, 49, 60, 70, 91];
-const series2 = [10, 20, 105, 40, 49, 50, 10, 67];
-const series = ref([
-  {
-    name: "series-1",
-    data: categories.map((c, i) => ({ x: c, y: series1[i] })),
-  },
-  {
-    name: "series-2",
-    data: categories.map((c, i) => ({ x: c, y: series2[i] })),
-  },
-]);
-const updateData = () => {
-  series.value = series.value.map((serie) => ({
-    ...serie,
-    data: serie.data.map((data) => ({
-      x: data.x,
-      y: Number(data.y) + Math.floor(Math.random() * 100),
-    })),
-  }));
-};
 onMounted(() => {
   const instance = getCurrentInstance();
   const vuetify = instance?.proxy?.$vuetify;
@@ -99,8 +114,8 @@ onMounted(() => {
 </script>
 <style>
 .apexcharts-tooltip.tooltip.apexcharts-theme-light {
-  background: #ff0000;
-  border: 1px solid #ff0000;
+  background: #ffff00;
+  border: 1px solid #ffff00;
   border-radius: 4px;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
   color: #373d3f;
